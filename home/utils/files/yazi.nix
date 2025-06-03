622e1932561611760a020b683eae418ebaf7bac3
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-03 12:11:41 +0100

    Modularized yazi

--- home/utils/files/yazi.nix
+++ home/utils/files/yazi.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    ./yazi
+    ./yazi/modules
   ];
 
   programs.yazi = {
---
+++ home/utils/files/yazi/modules/files.nix
@@ -0,0 +1,12 @@
+{ pkgs }:
+
+{
+  # Config
+  "yazi/yazi.toml".source = ../yazi.toml;
+  "yazi/theme.toml".source = ../theme.toml;
+  "yazi/keymap.toml".source = ../keymap.toml;
+  "yazi/catppuccin-mocha.tmTheme".source = ../../bat/catppuccin-mocha.tmTheme;
+
+  # Custom plugins
+  "yazi/plugins/markdown.sh" = import ../plugins/markdown-preview.nix { inherit pkgs; };
+}
