673bf760d6474a86d33325e2835f2c8d8ffb1755
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-16 17:13:50 +0100

    Added markdown-previewer to nvim

--- home/apps/editors/nvim/ftplugin/markdown.lua
+++ home/apps/editors/nvim/ftplugin/markdown.lua
@@ -1,3 +1,4 @@
+-- PDF export and viewing
 vim.keymap.set("n", ";pe", ":!pandoc % -o %:r.pdf<CR>", {
 	buffer = true,
 	desc = "Markdown: Export to PDF",
@@ -6,4 +7,20 @@ vim.keymap.set("n", ";pe", ":!pandoc % -o %:r.pdf<CR>", {
 vim.keymap.set("n", ";pv", ":!zathura %:r.pdf &<CR>", {
 	buffer = true,
 	desc = "Markdown: View PDF live",
+})
+
+-- Markdown preview (browser-based)
+vim.keymap.set("n", ";mp", "<Plug>MarkdownPreview", {
+	buffer = true,
+	desc = "Markdown: Preview in browser",
+})
+
+vim.keymap.set("n", ";ms", "<Plug>MarkdownPreviewStop", {
+	buffer = true,
+	desc = "Markdown: Stop browser preview",
+})
+
+vim.keymap.set("n", ";mt", "<Plug>MarkdownPreviewToggle", {
+	buffer = true,
+	desc = "Markdown: Toggle browser preview",
 })
