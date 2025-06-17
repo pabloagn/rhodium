b3ac6f7757ac19b31f54918faaf8e1c594d8395b
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-17 13:45:02 +0100

    Added hyprlock and other niceties

--- home/desktop/wm/niri.nix
+++ home/desktop/wm/niri.nix
@@ -1,6 +1,6 @@
 {...}: {
   xdg.configFile."niri/config.kdl" = {
     source = ./niri/config.kdl;
-    backup = true; # This is so that we don't break niri while switching
+    force = true; # This is so that we don't break niri while switching
   };
 }
