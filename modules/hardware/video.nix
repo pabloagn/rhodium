9987e846b747e9cab5f13f09b0e3be71025aff31
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-22 01:19:05 +0100

    Fixed gamma & temp correction util. Sticking with gammastep

--- modules/hardware/video.nix
+++ modules/hardware/video.nix
@@ -3,6 +3,7 @@
     brightnessctl # Read & control device brightness
     wdisplays # GUI for exploring and setting monitor options
     gammastep # Gamma & temperature set for wayland
+    geoclue2 # Geolocation framework for gammastep
     wlsunset # Gamma & temperature set for wayland supporting wlr-gamma-control-unstable-v1
     ddcutil # Gamma & temperature set fallback for hardware control
   ];
