7671e3fdf9b3f0463df7c5c20516463cd773377e
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-26 13:34:21 +0100

    CHORE: Pass niri config to declarative module

--- home/desktop/wm/niri.nix
+++ home/desktop/wm/niri.nix
@@ -2,8 +2,8 @@
   home.packages = with pkgs; [
     wl-mirror # Mirror client for wayland (Niri does not support mirroring)
   ];
-  # xdg.configFile."niri/config.kdl" = {
-  #   source = ./niri/config.kdl;
-  #   force = true;
-  # };
+  xdg.configFile."niri/config.kdl" = {
+    source = ./niri/config.kdl;
+    force = true;
+  };
 }
