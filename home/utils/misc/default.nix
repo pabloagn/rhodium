2cacb82b83a77774cac15bba04bf955f077528fe
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-06-28 18:16:18 +0100

    feat: completed fastfetch customization

--- home/utils/misc/default.nix
+++ home/utils/misc/default.nix
@@ -1,9 +1,12 @@
 {pkgs, ...}: {
+  imports = [
+    ./fastfetch.nix
+  ];
+
   home.packages = with pkgs; [
     via # GUI for adjusting RGB lighting
     cowsay # Generate ASCII pictures using a cow
     # disfetch # Neofetch successor
-    fastfetch # Faster disfetch
     ascii # Interactive ASCII name and symbol chart
   ];
 }
---
+++ home/utils/misc/fastfetch.nix
@@ -0,0 +1,10 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    fastfetch # Faster disfetch
+  ];
+
+  xdg.configFile."fastfetch/config.jsonc" = {
+    source = ./fastfetch/config.jsonc;
+    force = true;
+  };
+}
