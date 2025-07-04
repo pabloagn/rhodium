b287b6d85bb36607733cdb82a23b779277a914ac
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-07-04 14:38:59 +0100

    feat: added Berkeley Mono Developer Font, Rh Edition

--- overlays/fonts.nix
+++ overlays/fonts.nix
@@ -1,12 +1,12 @@
 final: prev: {
-  custom-fonts = prev.stdenv.mkDerivation rec {
+  rhodium-fonts = prev.stdenv.mkDerivation rec {
     pname = "rhodium-fonts";
     version = "1.0";
     src = ../assets/fonts;
     installPhase = ''
       mkdir -p $out/share/fonts/{opentype,truetype}
-      cp -v *.otf $out/share/fonts/opentype/ 2>/dev/null || true
-      cp -v *.ttf $out/share/fonts/truetype/ 2>/dev/null || true
+      find . -name "*.otf" -exec install -Dm644 {} $out/share/fonts/opentype/{} \;
+      find . -name "*.ttf" -exec install -Dm644 {} $out/share/fonts/truetype/{} \;
     '';
   };
 }
