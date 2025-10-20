b2419485c38c631a8ac1e7f3276f35a2cb036914
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2025-10-20 10:48:10 +0100

    feat(1password): updated to unstable channel (not beta)

--- home/security/auth/password-managers.nix
+++ home/security/auth/password-managers.nix
@@ -7,7 +7,8 @@
   home.packages = [
     # --- 1Password ---
     # pkgs._1password-gui
-    pkgs-unstable._1password-gui-beta # NOTE: Required for now since wayland clipboard is in Beta version
+    # pkgs-unstable._1password-gui-beta # NOTE: Required for now since wayland clipboard is in Beta version
+    pkgs-unstable._1password-gui # NOTE: Current 8.11 follows unstable
     pkgs._1password-cli
 
     # --- Bitwarden ---
