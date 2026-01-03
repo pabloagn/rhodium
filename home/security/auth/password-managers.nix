f1c42a1ee81a42efbf3dfa831f17e8178f025fd2
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2026-01-03 18:36:47 +0000

    feat(nixos-2511): checkpoint before bump

--- home/security/auth/password-managers.nix
+++ home/security/auth/password-managers.nix
@@ -8,7 +8,7 @@
     # --- 1Password ---
     # pkgs._1password-gui
     # pkgs-unstable._1password-gui-beta # NOTE: Required for now since wayland clipboard is in Beta version
-    pkgs-unstable._1password-gui # NOTE: Current 8.11 follows unstable
+    pkgs._1password-gui # NOTE: Bumped to 8.11 from stable
     pkgs._1password-cli
 
     # --- Bitwarden ---
