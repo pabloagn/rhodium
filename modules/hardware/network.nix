ef693e3591eae6ccf193102f98458870f62dea48
Pablo Aguirre <pabloaguirrenck@protonmail.com>
2026-03-19 20:54:59 +0100

    fix(protonvpn): added openvpn plugin to networkmanager, fixed protonvpn gui

--- modules/hardware/network.nix
+++ modules/hardware/network.nix
@@ -5,6 +5,7 @@
     firewalld # Firewall daemon with D-Bus interface
     firewalld-gui # Graphical interface for firewalld
     networkmanager # Network configuration & management CLI tool
+    networkmanager-openvpn # OpenVPN plugin for NetworkManager (required by ProtonVPN)
     networkmanagerapplet # GUI for setting up WiFi & Bluetooth
     wireguard-tools # Tools for the WireGuard secure network tunnel
     wireguard-ui # Web user interface to manage WireGuard setup
@@ -16,5 +17,8 @@
 
   networking = {
     firewall.checkReversePath = lib.mkForce "loose"; # HACK: Required for VPN
+    networkmanager.plugins = with pkgs; [
+      networkmanager-openvpn # Required by ProtonVPN GUI
+    ];
   };
 }
